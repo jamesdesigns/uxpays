a39dce9c49bf17545598be602f031494e0952d8f
James Hooper <jameshooperdesigns@gmail.com>
2024-09-02 22:43:12 -0600

    Add a color gradient to the h1 / h2 tag

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -102,9 +102,12 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			<div className="container mx-auto relative isolate overflow-hidden  py-24 sm:py-32">
 				<div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
 					<div className="mx-auto max-w-2xl lg:mx-0">
-						<h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
+						<h1 className="text-4xl font-bold bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 bg-clip-text text-transparent">
+						{project.title}
+      			</h1>
+						{/* <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
 							{project.title}
-						</h1>
+						</h1> */}
 						<p className="mt-6 text-lg leading-8 text-zinc-300">
 							{project.description}
 						</p>
--- app/projects/article.tsx
+++ app/projects/article.tsx
@@ -28,9 +28,12 @@ export const Article: React.FC<Props> = ({ project, views }) => {
 						{Intl.NumberFormat("en-US", { notation: "compact" }).format(views)}
 					</span>
 				</div>
-				<h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
+				<h2 className="text-4xl font-bold bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 bg-clip-text text-transparent">
+						{project.title}
+      	</h2>
+				{/* <h2 className="z-20 text-xl font-medium duration-1000 lg:text-3xl text-zinc-200 group-hover:text-white font-display">
 					{project.title}
-				</h2>
+				</h2> */}
 				<p className="z-20 mt-4 text-sm  duration-1000 text-zinc-400 group-hover:text-zinc-200">
 					{project.description}
 				</p>
