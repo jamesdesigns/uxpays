ecca252b04320d94f50fa2ef6ec6069a7db1525d
James Hooper <jameshooperdesigns@gmail.com>
2024-09-03 22:04:45 -0600

    Edit projects description

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -47,7 +47,7 @@ export default async function ProjectsPage() {
             Projects
           </h2>
           <p className="mt-4 text-zinc-400">
-          Explore my portfolio, where every project is carefully crafted to meet unique business needs. If you like what you see, let's work together and help your business grow.
+          Explore my portfolio, where every project is carefully crafted to meet the highest standards one would expect. If you like what you see, let's connect.
           </p>
           <a
           href="/contact"
