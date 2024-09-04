7a4dbb6a9bc9b74ca8c9e9731d01ebd0afb072a5
James Hooper <jameshooperdesigns@gmail.com>
2024-09-03 20:34:40 -0600

    Add changes

--- app/page.tsx
+++ app/page.tsx
@@ -42,7 +42,7 @@ export default function Home() {
           href="/contact"
           className="inline-block my-10 px-8 py-3 text-lg font-semibold text-white bg-blue-600 rounded-full hover:bg-blue-600 focus:ring-4 focus:ring-blue-300 focus:outline-none transition duration-300 fancy "
         >
-          Hire Me
+           Hire Me
         </a>
       </div>
     </div>
