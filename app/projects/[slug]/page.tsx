d6c3e04a2628ab3c82648c259b4567098b07403f
James Hooper <jameshooperdesigns@gmail.com>
2024-09-03 20:38:13 -0600

    Add changes

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -25,6 +25,11 @@ export async function generateStaticParams(): Promise<Props["params"][]> {
 }
 
 export default async function PostPage({ params }: Props) {
+  const article = await
+  getArticleById(articleId);
+  if (!article) {
+    return { notFound: true };
+  }
   const slug = params?.slug;
   const project = allProjects.find((project) => project.slug === slug);
 
