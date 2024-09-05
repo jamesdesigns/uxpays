22ccd861ff7bb81e4c7730d5a379661adff2434d
James Hooper <jameshooperdesigns@gmail.com>
2024-09-04 18:09:56 -0600

    Add text color to articles

--- app/projects/[slug]/header.tsx
+++ app/projects/[slug]/header.tsx
@@ -102,7 +102,7 @@ export const Header: React.FC<Props> = ({ project, views }) => {
 			<div className="container mx-auto relative isolate overflow-hidden  py-24 sm:py-32">
 				<div className="mx-auto max-w-7xl px-6 lg:px-8 text-center flex flex-col items-center">
 					<div className="mx-auto max-w-2xl lg:mx-0">
-						<h1 className="text-4xl font-bold bg-gradient-to-r from-blue-500 via-green-500 to-purple-500 bg-clip-text text-transparent">
+						<h1 className="text-4xl font-bold bg-gradient-to-r from-blue-500 to-pink-500 hover:text-white bg-clip-text text-transparent">
 						{project.title}
       			</h1>
 						{/* <h1 className="text-4xl font-bold tracking-tight text-white sm:text-6xl font-display">
